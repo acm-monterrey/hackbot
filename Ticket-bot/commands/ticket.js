011275bbf9055253b0437b4a98f731e2de086298
Alejandro Gonzalez <alejandro.g.carrillo@hotmail.com>
2021-02-17 14:10:10 -0600

    DM if a ticket already is created

--- Ticket-bot/commands/ticket.js
+++ Ticket-bot/commands/ticket.js
@@ -4,12 +4,23 @@ module.exports = {
     name: 'ticket',
     description: "Ticket system for the HackMTY",
     execute(Discord, message, args, client){
+        //Variables
         const canal = '811517697988755486';
         const cat = '811503336766832650';
         const ch = message.guild.channels.cache.find(ch => ch.name === message.author.id);
+        // Embed System
+        // let embed = new Discord.MessageEmbed()
+        //     .setTitle('Ticket System')
+        //     .setDescription('Esta es una descripcion')
+        //     .setFooter('Ni idea como se vea esto')
+        // message.channel.send(embed).then((msg) => {
+        //     msg.react('👀');
+        // })
+
+
         if(message.channel.id === canal) {
             if(ch){
-                return message.channel.send(`You already have a ticket open ${message.author}.`);
+                return message.author.send(`You already have a ticket open ${message.author}. Your ticket is in channel ${message.guild.channels.cache.find(c => c.name === message.author.id)}`);
             } else {
                 message.guild.channels.create(`${message.author.id}`).then( chanel => {
                     chanel.setParent(cat);
--- Ticket-bot/events/client/ready.js
+++ Ticket-bot/events/client/ready.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js')
+
 module.exports = () => {
     console.log('HackMTY Bot is now online!')
 }
