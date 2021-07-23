b8cd57b524b8e78d5e4fd97102567b0af5d56815
Alejandro Gonzalez <adgonzalezcarrillo+r@gmail.com>
2021-07-23 18:58:52 -0500

    Erase python part

--- cogs/cleanup.py
+++
@@ -1,4 +0,0 @@
-# TODO Easy after-event cleanup and also during event cleanup.
-
-# TODO Things like automatic purge of unused voice channels, unused text channels, unused ticket channels
-#   Remove excessive reactions from a post and AFK user management.
